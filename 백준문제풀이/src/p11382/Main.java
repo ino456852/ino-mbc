2e5a34b2a4fabb03e1f4998895f294b1d66cb185
ino456852 <ino456852@gmail.com>
2025-04-25 18:10:05 +0900

    [ADD] 백준문제풀이

---
+++ 백준문제풀이/src/p11382/Main.java
@@ -0,0 +1,19 @@
+package p11382;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+
+		String num = sc.next();
+		
+		String sum = num.toString().replace(" ", "+");
+		
+		
+		System.out.println(sum);
+	}
+
+}
