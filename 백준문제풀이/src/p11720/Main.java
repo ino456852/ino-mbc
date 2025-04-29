4e74d71983914bbb432786c1c9ccda5a559b75d7
ino456852 <ino456852@gmail.com>
2025-04-29 09:33:54 +0900

    [ADD] 백준 문제풀이

---
+++ 백준문제풀이/src/p11720/Main.java
@@ -0,0 +1,19 @@
+package p11720;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int num = sc.nextInt();
+		String s = sc.next();
+		int sum = 0;
+		for (int i = 0; i < num; i++) {
+			sum += s.charAt(i) - '0';
+		}
+		System.out.println(sum);
+	}
+}
